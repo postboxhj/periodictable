8eebf28853035fa267ba508aaac10dfec4acca75
Harish-sarkar <postboxhj@gmail.com>
2024-03-30 14:46:31 +0530

    shimmer changed

--- src/Components/Shimmer.js
+++ src/Components/Shimmer.js
@@ -1,66 +1,63 @@
 const Shimmer = () => {
     return (
       <div className="shimmer-card-container mx-16 px-10 flex flex-wrap">
-        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-800 shadow-md w-80">
-          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-600 rounded-md mb-4"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
-        </div>
-        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-800 shadow-md w-80">
-          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-600 rounded-md mb-4"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+        
+        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-300 shadow-md w-80">
+          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-300 rounded-md mb-4"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
         </div>
-        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-800 shadow-md w-80">
-          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-600 rounded-md mb-4"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
-        </div>
-        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-800 shadow-md w-80">
-          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-600 rounded-md mb-4"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-300 shadow-md w-80">
+          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-300 rounded-md mb-4"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
         </div>
-        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-800 shadow-md w-80">
-          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-600 rounded-md mb-4"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
-        </div>
-        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-800 shadow-md w-80">
-          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-600 rounded-md mb-4"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-300 shadow-md w-80">
+          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-300 rounded-md mb-4"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
         </div>
-        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-800 shadow-md w-80">
-          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-600 rounded-md mb-4"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
-          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-300 shadow-md w-80">
+          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-300 rounded-md mb-4"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
         </div>
-        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-800 shadow-md w-80">
-          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-600 rounded-md mb-4"></div>
+        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-300 shadow-md w-80">
+          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-300 rounded-md mb-4"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
+        </div>
+        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-300 shadow-md w-80">
+          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-300 rounded-md mb-4"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
+        </div>
+        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-300 shadow-md w-80">
+          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-300 rounded-md mb-4"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
+        </div>
+        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-300 shadow-md w-80">
+          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-300 rounded-md mb-4"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
+        </div>
+        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-300 shadow-md w-80">
+          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-300 rounded-md mb-4"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
+          <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
+        </div>
+        <div className="shimmer-card p-4 rounded-lg bg-white dark:bg-gray-300 shadow-md w-80">
+          <div className="shimmer-image h-40 w-full bg-gray-300 dark:bg-gray-300 rounded-md mb-4"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md mb-2"></div>
           <div className="shimmer-text h-4 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
