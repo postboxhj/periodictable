24e6bd3373935e1bc2b962b956ba6003bfea2fa9
Harish-sarkar <postboxhj@gmail.com>
2024-04-18 17:46:32 +0530

    Sort feature added

--- src/Components/ElementsList.js
+++ src/Components/ElementsList.js
@@ -6,7 +6,9 @@ const ElementsList = () => {
   const elementsList = useElement();
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
- 
+  const [groupBlockSort, setGroupBlockSort] = useState("");
+
+
   if (elementsList.length === 0) {
     return <h1>Loading...</h1>;
   }
@@ -16,6 +18,16 @@ const ElementsList = () => {
       elem.name.toLowerCase().startsWith(searchQuery.toLowerCase())
     );
     setSearchResults(filterSearch);
+  };
+  const handleGroupBlockSort = (event) => {
+    setGroupBlockSort(event.target.value);
+  };
+  const sortElements = () => {
+    if (groupBlockSort !== "") {
+      return elementsList.filter((elem) => elem.groupBlock === groupBlockSort);
+    } else {
+      return elementsList;
+    }
   };
   return (
     <div className="p-10">
@@ -34,14 +46,32 @@ const ElementsList = () => {
         >
           Search
         </button>
-        
+        <div>
+          <select
+            value={groupBlockSort}
+            onChange={handleGroupBlockSort}
+            className="shadow-lg bg-blue-200 font-semibold rounded-lg p-2"
+          >
+           <option value="" selected>All</option>
+            <option value="metal">Metal</option>
+            <option value="nonmetal">NonMetal</option>
+            <option value="alkali metal">Alkali Metal</option>
+            <option value="metalloid">Metalloid</option>
+            <option value="actinoid">Actinoid</option>
+            <option value="halogen">Halogen</option>
+            <option value="noble gas">Noble Gas</option>
+            <option value="transition metal">Transition Metal</option>
+            <option value="post-transition metal">Post-transition metal</option>
+          </select>
+        </div>
       </div>
+
       <div className="flex m-auto p-10 flex-wrap">
         {searchResults.length > 0
           ? searchResults.map((element) => (
               <ElementCard key={element.atomicNumber} data={element} />
             ))
-          : elementsList.map((element) => (
+          : sortElements().map((element) => (
               <ElementCard key={element.atomicNumber} data={element} />
             ))}
       </div>
